715e631b0cc22cc4a0f109ce82913d264d743da1
Tushar Saxena <stushar345@gmail.com>
2021-07-19 22:48:49 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -15,80 +15,48 @@ int shortest_cycle(int n)
 {
 	int ans = INT_MAX;
 
-	for (int i = 0; i < n; i++) {
-
-		// Make distance maximum
+	for (int i = 0; i < n; i++) //making every node as source node and doing bfs because shortest cycle can be identified if the source node is a part of cycle
+    {
 		vector<int> dist(n, INT_MAX);
-
-		// Take a imaginary parent
-		vector<int> par(n, -1);
-
-		// Distance of source to source is 0
 		dist[i] = 0;
-		queue<int> q;
 
-		// Push the source element
+		queue<int> q;
 		q.push(i);
 
-		// Continue until queue is not empty
-		while (!q.empty()) {
-
-			// Take the first element
+		while (!q.empty()) 
+        {
 			int x = q.front();
 			q.pop();
 
-			// Traverse for all it's childs
 			for (int child : gr[x]) 
 			{
-
-				// If it is not visited yet
 				if (dist[child] == INT_MAX) 
 				{
-
-					// Increase distance by 1
 					dist[child] = 1 + dist[x];
-
-					// Change parent
-					par[child] = x;
-
-					// Push into the queue
 					q.push(child);
 				}
-
-				// If it is already visited
-				else if (par[x] != child and par[child] != x)
+				else if (dist[child]>=dist[x])   //if any child is visited and has distance greater than or equal to parent then it is a backedge
 					ans = min(ans, dist[x] + dist[child] + 1);
 			}
 		}
 	}
 
-	// If graph contains no cycle
 	if (ans == INT_MAX)
 		return -1;
-
-	// If graph contains cycle
 	else
 		return ans;
 }
 
-// Driver code
 int main()
 {
-	// Number of vertices
-	int n = 7;
- 
-    // Add edges
-    Add_edge(0, 6);
-    Add_edge(0, 5);
-    Add_edge(5, 1);
-    Add_edge(1, 6);
-    Add_edge(2, 6);
-    Add_edge(2, 3);
+	int n = 5;
+
+    Add_edge(0, 1);
+    Add_edge(0, 2);
+    Add_edge(1, 3);
+    Add_edge(1, 4);
     Add_edge(3, 4);
-    Add_edge(4, 1);
+    Add_edge(4, 2);
 
-	// Function call
 	cout << shortest_cycle(n);
-
-	return 0;
 }
